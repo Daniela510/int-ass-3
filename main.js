4d7447715ad7420bdca64ab9564e26bf668a3bf9
Daniela510 <43034761+Daniela510@users.noreply.github.com>
2022-01-11 16:11:52 +0100

    arrow up

---
+++ main.js
@@ -0,0 +1,16 @@
+let arrow = document.getElementById("arrow");
+
+window.addEventListener("scroll", handleScroll);
+
+function handleScroll(e) {
+  let scDistance = window.pageYOffset || document.documentElement.scrollTop;
+    if (scDistance > 200) {
+      // user scrolled down more than 200 pixels
+      // change the position of the arrow on to visible area
+      arrow.style.transform = "translate(0,-13vw)"; 
+    } else {
+      // else they are back on the landing element already
+      // place arrow back on the default hidden position
+      arrow.style.transform = "translate(0,0)";
+    } 
+  }
